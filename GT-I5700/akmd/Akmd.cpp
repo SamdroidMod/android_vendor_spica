7009cb600400e048a891e19625563a5f06a0b99e
LeshaK <leshak@leshak.ru>
2010-12-03 20:33:52 +0300

    fixed akmd & sensors (criminal/CM)

--- GT-I5700/akmd/Akmd.cpp
+++ GT-I5700/akmd/Akmd.cpp
@@ -55,12 +55,11 @@ void Akmd::fill_result_vector(Vector o, Vector a, Vector m, short temperature, s
     out[6] =   roundf(a.x);
     out[7] =   roundf(a.z);
     out[8] =  -roundf(a.y);
-    
-    
-    
+
+    /* after a days of tests... */
     out[9] =   roundf(m.x);
-    out[10] =  roundf(m.y);
-    out[11] = -roundf(m.z);
+    out[10] = -roundf(m.y);
+    out[11] =  roundf(m.z);
 }
 
 /****************************************************************************/
@@ -68,7 +67,7 @@ void Akmd::fill_result_vector(Vector o, Vector a, Vector m, short temperature, s
 /****************************************************************************/
 void Akmd::sleep_until_next_update()
 {
-    int delay = 200; /* Based on SensorManager.SENSOR_DELAY_NORMAL */
+    int delay = 300; /* Based on SensorManager.SENSOR_DELAY_NORMAL */
     int candidate_delay;
 
     ChipReader* chips[3] = { magnetometer_reader, accelerometer_reader, temperature_reader };
@@ -91,7 +90,7 @@ void Akmd::sleep_until_next_update()
      * possible.
      */
     if (delay <= 60) {
-        delay = 21;
+        delay = 60;
     } else {
         /* divide by 2 to get 2 real samples for the slow modes too. */
         delay /= 2;
--- GT-I5700/akmd/device/BMA020.cpp
+++ GT-I5700/akmd/device/BMA020.cpp
@@ -11,17 +11,20 @@ namespace akmd {
 BMA020::BMA020()
 : index(0), accelerometer(3600)
 {
+    accelerometer.reset(false);
     abuf[0] = abuf[1] = Vector();
     unsigned char param=0;
     
-    fd = open(BMA020_NAME, O_RDONLY);
+    fd = open(BMA020_NAME, O_RDWR);
     SUCCEED(fd != -1);
     
     param = BMA020_RANGE_2G;
-    SUCCEED(ioctl(fd, BMA020_SET_RANGE,     &param) == 0);
+    //SUCCEED(ioctl(fd, BMA020_SET_RANGE, &param) == 0);
+    ioctl(fd, BMA020_SET_RANGE, &param);
     
     param = BMA020_BW_50HZ;
-    SUCCEED(ioctl(fd, BMA020_SET_BANDWIDTH, &param) == 0);
+    //SUCCEED(ioctl(fd, BMA020_SET_BANDWIDTH, &param) == 0);
+    ioctl(fd, BMA020_SET_BANDWIDTH, &param);
 }
 
 BMA020::~BMA020()
@@ -77,7 +80,8 @@ void BMA020::measure()
 
     bma020acc_t accels;
     
-    SUCCEED(ioctl(fd, BMA020_READ_ACCEL_XYZ, &accels) == 0);
+    //SUCCEED(ioctl(fd, BMA020_READ_ACCEL_XYZ, &accels) == 0);
+    ioctl(fd, BMA020_READ_ACCEL_XYZ, &accels);
 
     abuf[index] = Vector(-accels.y, accels.x, accels.z);
 
@@ -95,13 +99,15 @@ Vector BMA020::read()
 void BMA020::start()
 {
     unsigned char bmode = BMA020_MODE_NORMAL;
-    SUCCEED(ioctl(fd, BMA020_SET_MODE, &bmode) == 0);
+    //SUCCEED(ioctl(fd, BMA020_SET_MODE, &bmode) == 0);
+    ioctl(fd, BMA020_SET_MODE, &bmode);
 }
 
 void BMA020::stop()
 {        
     unsigned char bmode = BMA020_MODE_SLEEP;
-    SUCCEED(ioctl(fd, BMA020_SET_MODE, &bmode) == 0);
+    //SUCCEED(ioctl(fd, BMA020_SET_MODE, &bmode) == 0);
+    ioctl(fd, BMA020_SET_MODE, &bmode);
 }
 
 }
